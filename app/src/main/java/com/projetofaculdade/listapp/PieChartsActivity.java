c8cf97bb10c24a6245f9dd35dc3dbe8582804954
Henrique Martinho <henrique.martinho@outlook.com>
2024-05-20 22:45:49 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/projetofaculdade/listapp/PieChartsActivity.java
@@ -0,0 +1,14 @@
+package com.projetofaculdade.listapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PieChartsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pie_charts);
+    }
+}
